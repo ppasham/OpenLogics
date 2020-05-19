4730bbc507048b45bb9cf91d350c6a2422603656
ppasham <pashamvrpr@dss.nyc.gov>
2020-05-18 20:46:18 -0400

    Deleted

--- Conference/conference-web/src/main/java/com/viplogics/conference/repositories/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.viplogics.conference.repositories;
-
-
-import com.viplogics.conference.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-}
--- Conference/conference-web/src/main/java/com/viplogics/conference/repositories/SpeakerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.viplogics.conference.repositories;
-
-import com.viplogics.conference.models.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
-}
