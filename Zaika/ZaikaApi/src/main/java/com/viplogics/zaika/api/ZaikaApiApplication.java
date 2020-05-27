7c20844ac3e7ba2ce71b996b95b12919195e3082
ppasham <pashamvrpr@dss.nyc.gov>
2020-05-27 14:47:24 -0400

    Added ZaikaApi Project

---
+++ Zaika/ZaikaApi/src/main/java/com/viplogics/zaika/api/ZaikaApiApplication.java
@@ -0,0 +1,13 @@
+package com.viplogics.zaika.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaikaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaikaApiApplication.class, args);
+	}
+
+}
---
+++ Zaika/ZaikaApi/src/test/java/com/viplogics/zaika/api/ZaikaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viplogics.zaika.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaikaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
